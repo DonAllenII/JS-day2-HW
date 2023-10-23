c55ca150598d3746e268ae0317ba5e5b0ee664a4
DonAllenII <don.a.alexanderii@gmail.com>
2023-10-23 15:36:16 -0400

    added description for refactor

--- js/hw.js
+++ js/hw.js
@@ -136,6 +136,13 @@ counter('alexander')
 
 
 // Code Wars Original JS Exercise//
+//Digital root is the recursive sum of all the digits in a number.//
+
+//Given n, take the sum of the digits of n. If that value has more than one digit, //
+//continue reducing in this way until a single-digit number is produced. The input //
+//will be a non-negative integer.//
+
+
 
 function digitalRoot(n) {
     //turn n to a string
