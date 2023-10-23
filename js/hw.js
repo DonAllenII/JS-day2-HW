6a9a7a83e6fbe51a4958dcaa04a6c9ad33ed60d6
DonAllenII <don.a.alexanderii@gmail.com>
2023-10-23 15:33:32 -0400

    added 2nd codewars refactor

--- js/hw.js
+++ js/hw.js
@@ -154,3 +154,77 @@ function digitalRoot(n) {
     }
     return parseInt(n);
   }
+
+
+  // Code Wars Refactor//
+  //Create a function taking a positive integer between 1 and 3999 (both included)//
+  //as its parameter and returning a string containing the Roman Numeral representation of that integer.//
+
+  //Modern Roman numerals are written by expressing each digit separately starting with the left most //
+  //digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: //
+  //1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 //
+  //uses each Roman symbol in descending order: MDCLXVI.//
+
+  const solution = (n) => {
+    let roman = "";
+    while (n >= 1000) {
+        roman += "M";
+        n -= 1000;
+    }
+    while (n >= 500) {
+        if (n >= 900) {
+            roman += "CM";
+            n -= 900;
+        } else {
+            roman += "D";
+            n -= 500;
+        }
+    }
+    while (n >= 100){
+        if (n >= 400) {
+            roman += "CD";
+            n -= 400;
+        } else {
+            roman += "C";
+            n -= 100;
+        }
+    }
+    while (n >= 50){
+        if (n >= 90) {
+            roman += "XC";
+            n -= 90;
+        } else {
+            roman += "L";
+            n -= 50;
+        }
+    }
+    while (n >= 10) {
+        if (n >= 40) {
+            roman += "XL";
+            n -= 40;
+        } else {
+            roman += "X";
+            n -= 10;
+        }
+    }
+    while (n >= 5) {
+        if (n == 9) {
+            roman += "IX";
+            n -= 9;
+        } else {
+            roman+= "V";
+            n -= 5;
+        }
+    }
+    while (n >= 1) {
+        if (n == 4) {
+            roman += "IV";
+            n -= 4;
+        } else {
+            roman += "I";
+            n -= 1;
+        }
+    }
+        
+    return roman;
+  }
